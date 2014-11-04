d40c0db4f6eb27c0d54c65232f008554069e382e
Aitor Oses <aitor.oses@gmail.com>
2014-11-04 13:59:48 +0100

    First code has been generated

--- main.go
+++ main.go
@@ -14,10 +14,24 @@ func main() {
 	schemaStore := m.GetSchemaStore()
 	fmt.Printf("main: %+v\n", *schemaStore.GetSchema())
 
+	// Obtain the schemas
 	schemas := m.GetSchemaStore().GetSchemas()
 	fmt.Printf("\nSchemas: %+v\n", schemas)
 
+	// Obtain the mapped structures
 	structs := m.GetSchemaMapper().GetMappedStructs()
+
+	// Print the struct objects
 	fmt.Printf("\nStructs: %+v\n", structs)
+	for _, s := range structs {
+		fmt.Printf("\t -%+v\n", s)
+		fmt.Printf("\t\t Fields: %+v\n", s)
+		for _, f := range s.Fields {
+			fmt.Printf("\t\t\t -%+v\n", f)
+		}
+
+		// Print the struct as code
+		fmt.Printf("\n%s", s.Code())
+	}
 
 }
